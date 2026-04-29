4d6b713a613b4abb4bf8ea5554c703a43a18b1ad
boluwatifejohnson01-cell <boluwatifejohnson01@gmail.com>
2026-04-29 14:04:23 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let refreshButton = document.querySelector("#refreshBtn");
+let adviceContent = document.querySelector("#refreshContent");
+let textNumber = document.querySelector("#textNumber");
+
+let getApi = async () => {
+  let adviceApi = await fetch("https://api.adviceslip.com/advice");
+  let convertedApi = await adviceApi.json();
+  console.log(convertedApi);
+
+  adviceContent.textContent = `${convertedApi.slip.advice}`;
+  textNumber.textContent = `ADVICE #${convertedApi.slip.id}`;
+};
+
+getApi();
+
+refreshButton.addEventListener("click", getApi);
